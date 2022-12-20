fc083cae15befd6e819fe92f6b7dce8bf85da59e
NotARealCaveman <95105955+NotARealCaveman@users.noreply.github.com>
2022-12-19 22:05:24 -0500

    adds finer grainularity to simulation sync - now syncs only the states so that the simulation may be sync'd during multiple phases or defereed until later with try_write
    change also makes the database a little more clear. The database will hold a simulation snapshot structure that holds the most up to date information about the simulation. the database will have various push and pull methods for the kinds of simulation data which may be pushed. will also be exploring this paradigm for the rendering side of things and terrain as well

--- ManifestGLPersistence/Runtime_Database.cpp
+++ ManifestGLPersistence/Runtime_Database.cpp
@@ -90,14 +90,13 @@ void Manifest_Persistence::SimThread(ManifestRuntimeDatabase& runtimeDatabase)
 		auto prediction = begin + simulation.simulationFrame * simInterval;						
 		for (auto object{ 0 }; object < nPhysicsObjects; ++object)			simulation.bodies.worldSpaces[object].field[13] = simulation.bodies.worldSpaces[object].field[13] + 1;
 		//copy results to snapshot
-		SimulationSnapshot* snapshot{ new SimulationSnapshot };
-		snapshot->simulationFrame = simulation.simulationFrame;	
-		snapshot->xformTable.tableEntries = snapshot->xformTable.tableSize = nPhysicsObjects;		
-		snapshot->xformTable.keys = new UniqueKey[nPhysicsObjects];
-		snapshot->xformTable.values = new Xform[nPhysicsObjects];
-		memcpy(snapshot->xformTable.values, simulation.bodies.worldSpaces, sizeof(Xform) * nPhysicsObjects);
+		XformTable* stateSnapshot{ new XformTable};	
+		stateSnapshot->tableEntries = stateSnapshot->tableSize = nPhysicsObjects;
+		stateSnapshot->keys = new UniqueKey[nPhysicsObjects];
+		stateSnapshot->values = new Xform[nPhysicsObjects];
+		memcpy(stateSnapshot->values, simulation.bodies.worldSpaces, sizeof(Xform) * nPhysicsObjects);
 		//sync end simulation results - handles memory cleanup	
-		runtimeDatabase.PushSimulation(snapshot);		
+		runtimeDatabase.PushStates(stateSnapshot);		
 		//update sim frame
 		simulation.simulationFrame++;				
 		//sleep if permissible
@@ -112,24 +111,22 @@ void Manifest_Persistence::RenderThread(ManifestRuntimeDatabase& runtimeDatabase
 	auto set = false;
 	if (!(set = runtimeDatabase.init.test()))
 		runtimeDatabase.init.wait(set, std::memory_order_relaxed);
-	MFu64 renderFrame = 0;
-	MFu64 simulationFrame = -1;
+	MFu64 renderFrame = 0;	
 	//represents the handle to the graphic resource which works with the world space data
 	auto nObjects = runtimeDatabase.geometryNodes.instancedNodeIDs.tableEntries;	nObjects = 2000;
 	Xform* instancedVBOHandle = new Xform[nObjects];
 	//sleep and predicition
 	auto frameInterval = std::chrono::duration<double>{ 1 / 144.0 };
 	auto begin = std::chrono::high_resolution_clock::now();	
-	SimulationSnapshot* snapshot{nullptr};
+	XformTable* stateSnapshot{nullptr};
 	for (;;)
 	{
 		auto prediction = begin + renderFrame * frameInterval;	
-		auto currentSimulation = snapshot;
+		auto currentStates = stateSnapshot;
 		//get current simulation data - update vbo if new
-		if (currentSimulation != (snapshot = runtimeDatabase.PullSimulation()))
-			memcpy(instancedVBOHandle, snapshot->xformTable.values, sizeof(Xform) * nObjects);
-			simulationFrame = snapshot->simulationFrame;		
-		DLOG(35, "Render Frame: " << renderFrame << " using Simulation Frame: " << simulationFrame << " simulation data: " << instancedVBOHandle[0].field[13]);	
+		if (currentStates != (stateSnapshot = runtimeDatabase.PullStates()))
+			memcpy(instancedVBOHandle, stateSnapshot->values, sizeof(Xform) * nObjects);		
+		DLOG(35, "Render Frame: " << renderFrame << " simulation data: " << instancedVBOHandle[0].field[13]);	
 		//update render frame
 		renderFrame++;
 		//sleep if permissible
--- ManifestGLPersistence/Runtime_Database.h
+++ ManifestGLPersistence/Runtime_Database.h
@@ -53,66 +53,68 @@ namespace Manifest_Persistence
 		MFu64 simulationFrame{ 0 };
 	};
 
-	//acts as a double buffer for the simulation to store shared data of a complete simulation frame 
+	//acts as a double buffer for the simulation to store shared data of a complete simulation frame 	
+
 	struct SimulationSnapshot
 	{
-		MFu64 simulationFrame;
-		XformTable xformTable;
+		XformTable* xformTable{nullptr};
 	};
+
 
 	//Currently exploring a push/pull paradigm for updating and centralizing shared game state in the runtime database
 	class ManifestRuntimeDatabase
 	{
 		private:	
-			//if present - new snapshot
-			std::atomic<SimulationSnapshot*> newSimulation{ nullptr };			
-		public:								
+			//if present - new snapshot					
+			std::atomic<XformTable*> newStates{ nullptr };
+		public:
 			ManifestRuntimeDatabase(const ManifestBinaryDatabase& binaryDatabase);	
-			//atomically pushes a new simulation to the database and cleans up unused simulation memory if present
-			void PushSimulation(SimulationSnapshot* snapshot)
-			{
-				SimulationSnapshot* prevSim = nullptr;
+			//atomically pushes a new simulation states to database and cleans up unused state memory if needed
+			void PushStates(XformTable* stateSnapshot)
+			{				
+				XformTable* prevStates{ nullptr };
 				//atomically check for old sim and store new
 				simulationLock.Write(std::function([&]()
 					{
-						prevSim = newSimulation.load(std::memory_order_acquire);
-						newSimulation.store(snapshot, std::memory_order_release);
+						prevStates = newStates.load(std::memory_order_acquire);
+						newStates.store(stateSnapshot, std::memory_order_release);
 					}));
 				//if previous sim found delete - renderer missed sim
-				if (prevSim)
+				if (prevStates)
 				{
-					delete[] prevSim->xformTable.keys;
-					delete[] prevSim->xformTable.values;
-					delete prevSim;
-					prevSim = nullptr;
+					delete[] prevStates->keys;
+					delete[] prevStates->values;
+					delete prevStates;
+					prevStates = nullptr;
 				}
 			}
-			//returns the currently committed simulation - if a new simulation has been pushed, atomically pulls update and cleans up previous simulation memory 
-			SimulationSnapshot* PullSimulation()
+			//returns currently committed simulation state - if a new state has been pushed, atomically pulls update and cleans up previous state memory 
+			XformTable* PullStates()
 			{
 				//return committed if nothing new present
-				if (!newSimulation.load(std::memory_order_acquire))
-					return committedSimulation;
+				if (!newStates.load(std::memory_order_acquire))
+					return committedSimulation.xformTable;
 
 				//new simulation present - store old simulation
-				auto prevSim = committedSimulation;
+				XformTable* prevStates{ nullptr };
 				//atomically updates simulation and removes flag
 				simulationLock.Read(std::function([&]()
 					{
-						committedSimulation = newSimulation.load(std::memory_order_acquire);//upate simulation	
-						newSimulation.store(nullptr, std::memory_order_release);//remove flag
+						prevStates = committedSimulation.xformTable;
+						committedSimulation.xformTable = newStates.load(std::memory_order_acquire);//upate simulation	
+						newStates.store(nullptr, std::memory_order_release);//remove flag
 					}));
 				//relase old memory if applicable 				
-				if (prevSim)
+				if (prevStates)
 				{//may explore option that requires sim to render
-					delete[] prevSim->xformTable.keys;
-					delete[] prevSim->xformTable.values;
-					delete prevSim;
+					delete[] prevStates->keys;
+					delete[] prevStates->values;
+					delete prevStates;
 				}
 
-				return committedSimulation;
+				return committedSimulation.xformTable;
 			}			
-			SimulationSnapshot* committedSimulation{ nullptr };
+			SimulationSnapshot committedSimulation;
 			GeometryNodes geometryNodes;
 			GeometryObjects geometryObjects;
 			Materials materials;			
