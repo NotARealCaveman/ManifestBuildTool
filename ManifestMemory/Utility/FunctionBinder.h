7d7cd2fdd85717ed663303c8198fd9a28b0eb5be
NotARealCaveman <95105955+NotARealCaveman@users.noreply.github.com>
2024-07-10 06:22:43 -0400

    removes clutter from debug logger

--- ManifestMemory/Utility/FunctionBinder.h
+++ ManifestMemory/Utility/FunctionBinder.h
@@ -14,21 +14,21 @@ namespace Manifest_Memory
 		FunctionBinder(Function&& function, Params&&... params)
 			:callable{ std::make_unique<Callable<Function,Params...>>(std::forward<Function>(function),std::forward<Params>(params)...) } 
 		{
-			DLOG({ CONSOLE_CYAN }, "Creating function binder:", this);
+			//DLOG({ CONSOLE_CYAN }, "Creating function binder:", this);
 		};
 		FunctionBinder(FunctionBinder&& other) : callable{ std::move(other.callable) }
 		{ 
-			DLOG({ CONSOLE_CYAN }, "move ctor function binder:", this, "other:", &other); 		
+			//DLOG({ CONSOLE_CYAN }, "move ctor function binder:", this, "other:", &other); 		
 		};
 		FunctionBinder& operator=(FunctionBinder&& other)
 		{
-			DLOG({ CONSOLE_CYAN }, "move operator= function binder:", this, "other:",&other);
+			//DLOG({ CONSOLE_CYAN }, "move operator= function binder:", this, "other:",&other);
 			callable = std::move(other.callable);
 			return *this; 
 		};
 		~FunctionBinder()
 		{
-			DLOG({ CONSOLE_CYAN }, "Destroying function binder:", this);
+			//DLOG({ CONSOLE_CYAN }, "Destroying function binder:", this);
 		}
 		void operator()()
 		{
@@ -49,14 +49,20 @@ namespace Manifest_Memory
 			Callable(Function&& function, Params&&... params)
 				: boundFunction{ std::bind(function,params...) }
 			{
-				DLOG({ CONSOLE_CYAN }, "Creating Callable:", this);
+				//DLOG({ CONSOLE_CYAN }, "Creating Callable:", this);
 			};
-			Callable(Callable&& other) : boundFunction{ std::move(other.boundFunction) } { DLOG({ CONSOLE_CYAN }, "move ctor callable:", this, "other:", &other); };
-			Callable operator=(Callable&& other) { DLOG({ CONSOLE_CYAN }, "operator= callable:", this, "other:", &other); return std::move(other); };
+			Callable(Callable&& other) : boundFunction{ std::move(other.boundFunction) } 
+			{ //DLOG({ CONSOLE_CYAN }, "move ctor callable:", this, "other:", &other); 
+			};
+			Callable operator=(Callable&& other)
+			{ //DLOG({ CONSOLE_CYAN }, "operator= callable:", this, "other:", &other); return std::move(other); 
+			};
 			Callable(const Callable&) = delete;
 			Callable& operator=(const Callable&) = delete;
 
-			~Callable() { DLOG({ CONSOLE_CYAN }, "Destroying callable:", this); };
+			~Callable()			
+			{ //DLOG({ CONSOLE_CYAN }, "Destroying callable:", this);
+			};
 			void operator()()
 			{
 				std::invoke(boundFunction);
