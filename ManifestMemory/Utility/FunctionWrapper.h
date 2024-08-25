5bb9adaba51e3334856b9c80db7da2153de19fd0
NotARealCaveman <95105955+NotARealCaveman@users.noreply.github.com>
2024-08-24 21:05:07 -0400

    Update FunctionWrapper.h
    no need for function wrapper

--- ManifestMemory/Utility/FunctionWrapper.h
+++ ManifestMemory/Utility/FunctionWrapper.h
@@ -9,44 +9,7 @@ using namespace Manifest_Utility;
 
 
 namespace Manifest_Memory
-{
-	struct FunctionWrapper
-	{
-		template<typename Function>
-		FunctionWrapper(Function function) : callable{ std::make_unique<Callable<Function>>(function) } {};
-
-		template<typename R, typename... Params>
-		using FunctionPointer = R(*)(Params...);
-
-		template<typename R, typename... Params>
-		decltype(auto) Invoke(Params... params) const
-		{
-			const Callable<FunctionPointer<R, Params...>>& functor{ reinterpret_cast<const Callable<FunctionPointer<R, Params...>>&>(*callable.get()) };
-			DLOG({ CONSOLE_BG_BLUE }, "function name:", typeid(functor).name());
-			return ForwardFunction(functor, params...);
-		}
-
-		struct Base
-		{};
-
-		template<typename Function>
-		struct Callable : public Base
-		{
-			Callable(Function _function) : function{ _function }
-			{};
-
-			template<typename... Params>
-			decltype(auto) operator()(Params... params) const
-			{
-				return ForwardFunction(function,params...);
-			}
-
-			Function function;
-		};
-
-		std::unique_ptr<Base> callable;
-	};
-
+{ 
 	struct FunctionWrapper_OLD
 	{
 	private:
